8205dd591b6473c65f45bc589459472eb4bc4fea
Jorge Gonzalez Mejia <jorgegonzalezmejia@MacBook-Air-de-Jorge.local>
2026-06-02 12:17:13 -0500

    refact: migrar a framework Astro, unificar layout global y reestructurar a paginas .astro

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  srcDir: './src',
+  publicDir: './public',
+  outDir: './dist',
+  build: {
+    format: 'file',
+  },
+});
